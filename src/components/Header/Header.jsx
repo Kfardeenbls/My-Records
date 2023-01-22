0f3d1bcf5b873652db3c5f0c6113f58807e0b0f0
Fardeen Khan <Kfardeenbls@gmail.com>
2023-01-22 13:15:59 +0530

    working in a new project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import React from "react";
+import classes from "./Header.module.css";
+import { FaUserCircle } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand className={classes.icon} href="#home">
+            My Records
+          </Navbar.Brand>
+          <Nav className={classes.pages}>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">My Records</Nav.Link>
+            <Nav.Link href="#pricing">Services</Nav.Link>
+            <Nav.Link href="#pricing">Contact us</Nav.Link>
+          </Nav>
+          <div className={classes.log}>
+            <button type="button" class="btn btn-outline-info">
+              Sign In
+            </button>
+            <span className={classes.space}>space </span>
+            <button type="button" className="btn btn-outline-info">
+              Sign Up
+            </button>
+            <span href="/" className={classes.userLogo}>
+              <FaUserCircle />
+            </span>
+          </div>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
