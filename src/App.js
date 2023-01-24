e890fb64924c5301dc156c186d2c6e9de7dcfb8b
Fardeen Khan <Kfardeenbls@gmail.com>
2023-01-25 00:24:29 +0530

    make some changes in header jsx

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import ContactUs from "./components/ContactUs/ContactUs";
+
 // import Header from "./components/Header/Header";
-// import SignUp from "./Features/SignUp";
-// import SignIn from "./Features/SignIn";
+import SignUp from "./Features/SignUp";
+import SignIn from "./Features/SignIn";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
+
+import Records from "./pages/records/Records";
+import Services from "./pages/services/Services";
+import Home from "./pages/Home/Home";
+import ContactUs from "./pages/ContactUs/ContactUs";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} exact></Route>
+          <Route path="/" element={<Home />} />
+          Routes
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/Records" element={<Records />} />
+          <Route path="/Services" element={<Services />} />
           {/* <SignUp /> */}
           {/* <SignIn /> */}
           {/* <ContactUs /> */}
